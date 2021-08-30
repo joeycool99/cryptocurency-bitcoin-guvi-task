abdcbaa5842efd41d6d810e8cbf5a88516beddda
joeycool99 <72965762+joeycool99@users.noreply.github.com>
2021-08-30 23:02:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+
+//         return res.json()
+//     })
+//     .then(data => {
+
+//         console.log(data.ticker.price)
+//     })
+//     .catch(e => {
+//         console.log("OH NO! ERROR!", e)
+//     })
+
+bitcoin = document.getElementById("bitcoin")
+const fetchBitcoinPrice = async () => {
+    try {
+        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
+        const data = await res.json();
+        var fin = (Math.trunc(data.ticker.price))
+
+        bitcoin.append(`₹${fin}`)
+
+    } catch (e) {
+        return ("SOMETHING WENT WRONG!!!", e)
+    }
+}
+
+fetchBitcoinPrice()
